19cac300048e6e244d987cee2737019ca6bcb6f4
himanshu12gupta <70939314+himanshu12gupta@users.noreply.github.com>
2023-09-29 16:01:21 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,11 @@
+"use strict";
+//alert(3 + 3) // we are using node js , not browser
+
+let name = "himanshu";
+let age = 9
+let isLogin = false 
+let x = null
+let empty = ""
+
+//
+console.table([typeof name, typeof age, typeof x,typeof empty, typeof undefined]);
---
+++ first.js
@@ -0,0 +1,6 @@
+const accountId = 1223
+let accountEmail = "hk@gmail.com"
+var accPass = "gupta@11"
+city = "Mathura"
+console.log(accountId);
+console.table([accountEmail,accPass,accountId]);
